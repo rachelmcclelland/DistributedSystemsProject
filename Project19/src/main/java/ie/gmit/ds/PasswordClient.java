2af50c015e95204e28df1f5ebeb604ef1cf4586e
Rachel <g00337231@gmit.ie>
2019-10-22 19:14:51 +0100

    server working with tester client

--- Project19/src/main/java/ie/gmit/ds/PasswordClient.java
+++ Project19/src/main/java/ie/gmit/ds/PasswordClient.java
@@ -1,5 +1,64 @@
 package ie.gmit.ds;
 
+import com.google.protobuf.BoolValue;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class PasswordClient {
 
-}
+    private static final Logger logger =
+            Logger.getLogger(PasswordClient.class.getName());
+    private final ManagedChannel channel;
+    // private final PasswordServiceGrpc.PasswordServiceStub asyncPasswordService;
+    private final PasswordServiceGrpc.PasswordServiceBlockingStub syncPasswordService;
+
+    public PasswordClient(String host, int port) {
+        channel = ManagedChannelBuilder
+                .forAddress(host, port)
+                .usePlaintext()
+                .build();
+        syncPasswordService = PasswordServiceGrpc.newBlockingStub(channel);
+    }
+
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    public void hashPasswword(HashRequest hr) {
+        logger.info("Hashing Password " + hr);
+        HashResponse hashResponse = null;
+
+        try {
+            // hashes the password
+            hashResponse = syncPasswordService.hash(hr);
+            logger.info(("Hashed Password: " + hashResponse.getHashedPassword()));
+        } catch (StatusRuntimeException ex) {
+            logger.log(Level.WARNING, "RPC failed: {0}", ex.getStatus());
+            return;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        PasswordClient pClient = new PasswordClient("10.12.7.50", 50551);
+
+        int userID = 123;
+        String password = "helloworld";
+
+        HashRequest hashRequest = HashRequest.newBuilder()
+                .setPassword(password)
+                .setUserID(userID)
+                .build();
+
+        try{
+            pClient.hashPasswword(hashRequest);
+        }finally {
+            // Don't stop process, keep alive to receive async response
+            Thread.currentThread().join();
+        }
+    }
+}// PasswordClient class
--- Project19/src/main/java/ie/gmit/ds/PasswordServiceImpl.java
+++ Project19/src/main/java/ie/gmit/ds/PasswordServiceImpl.java
@@ -1,6 +1,7 @@
 package ie.gmit.ds;
 
 import com.google.protobuf.BoolValue;
+import com.google.protobuf.ByteString;
 import io.grpc.stub.StreamObserver;
 
 import java.util.logging.Logger;
@@ -11,16 +12,51 @@ public class PasswordServiceImpl extends PasswordServiceGrpc.PasswordServiceImpl
             Logger.getLogger(PasswordServiceImpl.class.getName());
 
     private String password;
+    private char[] passwordChar;
     private int userId;
+    private byte[] salt;
+    private byte[] hashedpassword;
+    private boolean validatePass;
 
     @Override
     public void hash(HashRequest request, StreamObserver<HashResponse> responseObserver) {
         // get info from the request
-        // request password
-         password = request.getPassword();
-         logger.info("Password is: " + password);
 
-         userId = request.getUserID();
+        try {
+            // request password
+            password = request.getPassword();
+
+            passwordChar = password.toCharArray();
+            // gets user id
+            userId = request.getUserID();
+
+            //gets the salt
+            salt = Passwords.getNextSalt();
+
+            // hash the password
+            hashedpassword = Passwords.hash(passwordChar, salt);
+
+            //logger.info("Password is: " + password);
+
+            HashResponse hr = HashResponse.newBuilder()
+                    .setSalt(ByteString.copyFrom(salt))
+                    .setHashedPassword(String.valueOf(hashedpassword))
+                    .setUserID(userId)
+                    .build();
+
+            validatePass = Passwords.isExpectedPassword(passwordChar, salt, hashedpassword);
+            System.out.println("User ID: " + userId + "Password: " + password + "Hashed Password: " + hashedpassword);
+            System.out.println(validatePass);
+
+        }
+        catch (RuntimeException e)
+        {
+            Logger.getLogger("error");
+        }
+
+
+
+
 
         // use john frenches code for hash
         // build repsonse using builder
